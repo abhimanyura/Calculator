a1468513df8edff59d07b298c6d56cb25729b8d6
abhimanyura <abhimanyu.ise@gmail.com>
2017-08-02 14:53:57 +0530

    local repo ommit2

---
+++ calculator/src/calculator/substraction.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class substraction {
+	public void substract()
+	{
+
+	int a,b,c;
+		 a=10;
+		 b=20;
+		c=a-b;
+		System.out.println("substraction Resut is"+c);
+	}
+}
